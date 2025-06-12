714ba30773643ad94736fa8f62a4a535435abcaa
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2025-06-12 20:38:09 -0300

    feat: início de projeto com entidades

---
+++ app/Http/Controllers/ResponsavelController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreResponsavelRequest;
+use App\Http\Requests\UpdateResponsavelRequest;
+use App\Models\Responsavel;
+
+class ResponsavelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreResponsavelRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Responsavel $responsavel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Responsavel $responsavel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateResponsavelRequest $request, Responsavel $responsavel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Responsavel $responsavel)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\AgendamentoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// implementar rota pra onde?
+// agendamento index
+
+Route::get('/', [AgendamentoController::class, 'index'])
+    ->name('agendamento.index');
